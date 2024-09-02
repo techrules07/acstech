8bec8123faf12b97fa95d95f5453100d7604d4fc
Robin Isac <robin.isac@thesocialbytes.com>
2024-09-02 10:36:57 +0530

    swagger_implementation

---
+++ src/main/java/com/acs/tech/SpringFoxConfig.java
@@ -0,0 +1,32 @@
+package com.acs.tech;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
+@ComponentScan
+public class SpringFoxConfig {
+
+    @Value("${baseUrl}")
+    private String host;
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .host(host)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
